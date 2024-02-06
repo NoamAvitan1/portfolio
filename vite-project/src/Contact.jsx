cdf14b9067f6cb208e459f4168dbbd7a0b999230
Noam Avitan <noamavitan159753@gmail.com>
2024-02-06 15:13:42 +0200

    .

--- vite-project/src/Contact.jsx
+++ vite-project/src/Contact.jsx
@@ -11,12 +11,12 @@ export default function Contact() {
       <h1 className='title'>Contact Me</h1>
         <section className='data'>
           <div className='contact-info'>
-            <span><CiMail style={{color:'white'}}/></span>
-           <p style={{color:'white'}}>noamavitan159753@gmail.com</p>
+            <span><CiMail style={{color:'white',fontSize:'20px'}}/></span>
+           <p style={{color:'white',fontSize:'20px'}}>noamavitan159753@gmail.com</p>
           </div>
           <div className='contact-info'>
-            <span><MdOutlineLocalPhone style={{color:'white'}}/></span>
-            <p style={{color:'white'}}>+972 53-723-4532</p>
+            <span><MdOutlineLocalPhone style={{color:'white',fontSize:'20px'}}/></span>
+            <p style={{color:'white', width:'250px', fontSize:'20px'}}>+972 53-723-4532</p>
           </div>
         </section>
     </article>
